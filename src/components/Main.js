163cb1f2e519df1f62ba6fea23f6bf6e679332d9
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-11-08 20:00:57 -0500

    react cheese front-end done

---
+++ src/components/Main.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+
+function Main(props) {
+  const [cheese, setCheese] = useState(null);
+
+  const URL = "http://localhost:4000/cheese/";
+
+  const getCheese = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setCheese(data);
+  };
+
+  const createCheese = async (cheeses) => {
+    // make post request to create cheeses
+    await fetch(URL, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cheeses),
+    });
+    // update list of people
+    getCheese();
+  };
+
+  useEffect(() => getCheese(), []);
+
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <Index cheese={cheese} createCheese={createCheese} />
+        </Route>
+        <Route
+          path="/cheese/:id"
+          render={(rp) => (
+            <Show
+              {...rp}
+            />
+          )}
+        />
+      </Switch>
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/pages/Index.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import {Link} from "react-router-dom"
+
+function Index(props) {
+  // state to hold formData
+  const [newForm, setNewForm] = useState({
+    name: "",
+    countryOfOrigin: "",
+    image: "",
+  });
+
+  // handleChange function for form
+  const handleChange = (event) => {
+    setNewForm({ ...newForm, [event.target.name]: event.target.value });
+  };
+
+  // handle submit function for form
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.createCheese(newForm);
+    setNewForm({
+      name: "",
+      countryOfOrigin: "",
+      image: "",
+    });
+  };
+
+  // loaded function
+  const loaded = () => {
+    return props.cheese.map((cheeses) => (
+      <div key={cheeses._id} className="cheeses">
+        <Link to={`/cheese/${cheeses._id}`}><h1>{cheeses.name}</h1></Link>
+        <img src={cheeses.image} alt={cheeses.name} />
+        <h3>{cheeses.title}</h3>
+      </div>
+    ));
+  };
+
+  const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newForm.name}
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.countryOfOrigin}
+          name="countryOfOrigin"
+          placeholder="countryOfOrigin"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={newForm.image}
+          name="image"
+          placeholder="image"
+          onChange={handleChange}
+        />
+        <input type="submit" value="Create Cheese" />
+      </form>
+      {props.cheese ? loaded() : loading()}
+    </section>
+  );
+}
+
+export default Index;
